91fda1e66d4279779b53ac6c6d68b57134b93dca
genboy <msg@genboy.net>
2017-11-14 23:34:16 +0100

    Added ClearLa and own fork of FloatingTexter

---
+++ FloatingTexter/src/FloatingTexter/RefreshTask.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace FloatingTexter;
+
+use pocketmine\scheduler\PluginTask;
+
+class RefreshTask extends PluginTask{
+
+    private $plugin;
+
+    public function __construct(Main $plugin){
+        $this->plugin = $plugin;
+        parent::__construct($plugin);
+    }
+
+    public function onRun(int $currentTick){
+        foreach($this->plugin->getServer()->getLevels() as $level){
+            foreach($level->getEntities() as $entity){
+                if($entity instanceof FloatingText){
+                    $entity->respawn();
+                }
+            }
+        }
+    }
+}
